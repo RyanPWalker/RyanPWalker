c7f5ad21f79448848ecd52dd83302521076cd619
Ryan Walker <rywalker@overstock.com>
2019-03-09 11:32:27 -0700

    Fixed mobile styling(hopefully) and added floating bubbles

--- src/components/About.js
+++ src/components/About.js
@@ -27,7 +27,24 @@ class About extends Component {
                             <FB />
                         </a>
                     </div>
-                    <img src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/53063689_10213847227635138_4767014043263172608_o.jpg?_nc_cat=111&_nc_ht=scontent-sjc3-1.xx&oh=ecb578125548cf0248adb87bf97c5f46&oe=5CDB97A2" alt="Me, my wife, and my dog Cody." />
+                    <div className="bubbles">
+                        <div>
+                            <img className="floating img1" src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/53628866_10213847274316305_2282122961399840768_n.jpg?_nc_cat=103&_nc_ht=scontent-sjc3-1.xx&oh=550ebe5f64b4037e52886d83ca18465b&oe=5CDFFB84" alt="Me" />
+                            <h3>Me</h3>
+                        </div>
+                        <div className="">
+                            <img className="floating img2" src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/52953592_10213847271196227_1904766005196881920_o.jpg?_nc_cat=105&_nc_ht=scontent-sjc3-1.xx&oh=de3800963e8ae8c98318564bc6d60a00&oe=5D105C99" alt="My Wife Maddy" />
+                            <h3>Maddy</h3>
+                        </div>
+                        <div className="">
+                            <img className="floating img3" src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/53495588_10213847271396232_8225031756760219648_o.jpg?_nc_cat=106&_nc_ht=scontent-sjc3-1.xx&oh=0481eb62d95bfdb7a7f931e74aa1c0ca&oe=5D1D03A2" alt="My crazy dog Cody" />
+                            <h3>Cody</h3>
+                        </div>
+                        <div className="">
+                            <img className="floating img4" src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/53514134_10213847276196352_944188902929858560_o.jpg?_nc_cat=103&_nc_ht=scontent-sjc3-1.xx&oh=0510f47feee65f5b0ba4be42a143a345&oe=5D26D60B" alt="My cuddly dog Willow" />
+                            <h3>Willow</h3>
+                        </div>
+                    </div>
                 </div>
             </Fragment>
         );
