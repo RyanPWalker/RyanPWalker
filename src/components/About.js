07093ff5c42c60de0f7f6c8eed5fd990630f21ef
Ryan Walker <ryan.walker@domo.com>
2025-01-29 17:30:14 -0700

    fix: update deps and info

--- src/components/About.js
+++ src/components/About.js
@@ -1,58 +1,96 @@
-import React, { Fragment } from 'react';
-import '../less/About.less';
+import React, { Fragment } from "react";
+import "../less/About.less";
 
 // SVG
-import Github from '../assets/images/social/github';
-import FB from '../assets/images/social/fb';
-import Email from '../assets/images/social/email';
-import LinkedIn from '../assets/images/social/linkedin';
+import Github from "../assets/images/social/github";
+import Email from "../assets/images/social/email";
+import LinkedIn from "../assets/images/social/linkedin";
 
 export default () => (
-    <Fragment>
-        <h2 className="about-title">About</h2>
-        <div className="about-container">
-            <div className="icons">
-                <a href="https://github.com/RyanPWalker" target="blank" title="My Github">
-                    <Github />
-                </a>
-                <a href="mailto:ryanprestonwalker@gmail.com" target="blank" title="Email me">
-                    <Email />
-                </a>
-                <a href="https://www.facebook.com/dyin.to.be.ryan" target="blank" title="My Facebook">
-                    <FB />
-                </a>
-                <a href="https://www.linkedin.com/in/ryan-preston-walker/" target="blank" title="My LinkedIn">
-                    <LinkedIn />
-                </a>
-            </div>
-            <div className="bubbles">
-                <div>
-                    <img className="floating img1" src="https://raw.githubusercontent.com/RyanPWalker/RyanPWalker/master/src/assets/images/Handsome.jpg" alt="Me" />
-                    <h3>Me</h3>
-                </div>
-                <div>
-                    <img className="floating img2" src="https://raw.githubusercontent.com/RyanPWalker/RyanPWalker/master/src/assets/images/Maddy.jpg" alt="My Wife Maddy" />
-                    <h3>Maddy</h3>
-                </div>
-                <div>
-                    <img className="floating img3" src="https://raw.githubusercontent.com/RyanPWalker/RyanPWalker/master/src/assets/images/cody.jpg" alt="My crazy dog Cody" />
-                    <h3>Cody</h3>
-                </div>
-                <div>
-                    <img className="floating img4" src="https://raw.githubusercontent.com/RyanPWalker/RyanPWalker/master/src/assets/images/willow.jpg" alt="My cuddly dog Willow" />
-                    <h3>Willow</h3>
-                </div>
-            </div>
-            <p>
-                Thanks for checking out my site! I've been a developer for 5 years now and still love it.{' '}
-                I'm currently working at Overstock.com as a Senior Front End Developer in Midvale, Utah.{' '}
-                I mostly work with Reactjs, Redux, Less, Webpack, and Nodejs on a daily basis.{' '}
-                In my free time I like to game, make music, and operate my side businesses{' '}
-                <a href="https://www.rhynosaxeandarchery.com/" target="blank" className="rainbow-link">Rhyno's Axe Throwing and Archery</a>
-                {' '}and{' '}
-                <a href="https://www.superiormarketingdesign.com" target="blank" className="rainbow-link">Make Me Superior</a>.{' '}
-                Please reach out and <a href="mailto:ryanprestonwalker@gmail.com" target="blank" title="Email me">connect</a>!
-            </p>
+  <Fragment>
+    <h2 className="about-title">About</h2>
+    <div className="about-container">
+      <div className="icons">
+        <a
+          href="https://github.com/RyanPWalker"
+          target="blank"
+          title="My Github"
+        >
+          <Github />
+        </a>
+        <a
+          href="mailto:ryanprestonwalker@gmail.com"
+          target="blank"
+          title="Email me"
+        >
+          <Email />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/ryan-preston-walker/"
+          target="blank"
+          title="My LinkedIn"
+        >
+          <LinkedIn />
+        </a>
+      </div>
+      <div className="bubbles">
+        <div>
+          <img
+            className="floating img1"
+            src="https://raw.githubusercontent.com/RyanPWalker/RyanPWalker/master/src/assets/images/Handsome.jpg"
+            alt="Me"
+          />
+          <h3>Me</h3>
         </div>
-    </Fragment>
+        <div>
+          <img
+            className="floating img2"
+            src="https://raw.githubusercontent.com/RyanPWalker/RyanPWalker/master/src/assets/images/Maddy.jpg"
+            alt="My Wife Maddy"
+          />
+          <h3>Maddy</h3>
+        </div>
+        <div>
+          <img
+            className="floating img3"
+            src="https://raw.githubusercontent.com/RyanPWalker/RyanPWalker/master/src/assets/images/cody.jpg"
+            alt="My crazy dog Cody"
+          />
+          <h3>Cody</h3>
+        </div>
+        <div>
+          <img
+            className="floating img4"
+            src="https://raw.githubusercontent.com/RyanPWalker/RyanPWalker/master/src/assets/images/willow.jpg"
+            alt="My cuddly dog Willow"
+          />
+          <h3>Willow</h3>
+        </div>
+      </div>
+      <p>
+        Thanks for checking out my site! I've been a developer for 10 years now
+        and still love it. I'm currently working at Domo as a Technical Lead,
+        Senior Software Developer in American Fork, Utah. I mostly work with
+        React.js, Redux, TypeScript, CSS Modules, Next.js, and Node.js on a
+        daily basis. In my free time I like to play with my kids, game, make
+        music, and operate my side business{" "}
+        <a
+          href="https://www.rhynosaxeandarchery.com/"
+          target="blank"
+          className="rainbow-link"
+        >
+          Rhyno's Axe Throwing and Archery
+        </a>
+        . Please reach out and{" "}
+        <a
+          href="mailto:ryanprestonwalker@gmail.com"
+          target="blank"
+          title="Email me"
+        >
+          connect
+        </a>
+        !
+      </p>
+    </div>
+  </Fragment>
 );
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,75 +1,125 @@
-import React, { Fragment } from 'react';
-import '../less/Skills.less';
+import React, { Fragment } from "react";
+import "../less/Skills.less";
 
 // SVG
-import Webpack from '../assets/images/webpack';
-import Babel from '../assets/images/babel';
-import Jquery from '../assets/images/jquery';
-import Triforce from '../assets/images/triforce';
-import Redux from '../assets/images/redux';
-import Expo from '../assets/images/expo';
-import Jest from '../assets/images/jest';
+import Webpack from "../assets/images/webpack";
+import Babel from "../assets/images/babel";
+import Jquery from "../assets/images/jquery";
+import Triforce from "../assets/images/triforce";
+import Redux from "../assets/images/redux";
+import Jest from "../assets/images/jest";
 
 export default () => (
-    <Fragment>
-        <h2 className="skills-title">Sweet Skills</h2>
-        <div className="skills-container">
-            <div>
-                <a href="https://reactjs.org/" target="blank">
-                    <i id="skills" title="React" className="devicon-react-original-wordmark colored "></i>
-                </a>
-                <a href="https://redux.js.org/" target="blank" title="Redux">
-                    <Redux />
-                </a>
-                <a href="https://webpack.js.org/" target="blank" title="Webpack">
-                    <Webpack />
-                </a>
-                <a href="https://babeljs.io/" target="blank" title="Babel">
-                    <Babel />
-                </a>
-            </div>
-            <div>
-                <a href="https://www.w3schools.com/js/" target="blank">
-                    <i id="skills" title="JavaScript" className="devicon-javascript-plain colored "></i>
-                </a>
-                <a href="https://www.w3schools.com/html/html_intro.asp" target="blank">
-                    <i id="skills" title="Html5" className="devicon-html5-plain-wordmark colored "></i>
-                </a>
-                <a href="https://www.w3schools.com/css/" target="blank">
-                    <i id="skills" title="Css3" className="devicon-css3-plain-wordmark colored "></i>
-                </a>
-                <a href="http://lesscss.org/" target="blank">
-                    <i id="skills" title="Less" className="devicon-less-plain-wordmark colored"></i>
-                </a>
-            </div>
-            <div>
-                <a href="https://nodejs.org" target="blank">
-                    <i id="skills" title="NodeJS" className="devicon-nodejs-plain colored"></i>
-                </a>
-                <a href="https://git-scm.com/" target="blank">
-                    <i id="skills" title="Git" className="devicon-git-plain-wordmark colored"></i>
-                </a>
-                <a href="https://jquery.com/" target="blank" title="Jquery">
-                    <Jquery />
-                </a>
-                <a href="https://wordpress.com" target="blank">
-                    <i id="skills" title="Wordpress/Web Builders" className="devicon-wordpress-plain"></i>
-                </a>
-            </div>
-            <div>
-                <a href="https://www.adobe.com/products/illustrator.html" target="blank">
-                    <i id="skills" title="Adobe Illustrator" className="devicon-illustrator-plain colored"></i>
-                </a>
-                <a href="https://jestjs.io/" target="blank" title="Jest">
-                    <Jest />
-                </a>
-                <a href="https://www.zelda.com/about/" target="blank" title="The Legend of Zelda">
-                    <Triforce />
-                </a>
-                <a href="https://www.drumeo.com/beat/3-studies-that-prove-drummers-are-the-best/" target="blank" title="Drum set" className="drums">
-                    <img src="http://www.drumchat.com/images/smilies/drumset-3.gif" id="skills" />
-                </a>
-            </div>
-        </div>
-    </Fragment>
+  <Fragment>
+    <h2 className="skills-title">Sweet Skills</h2>
+    <div className="skills-container">
+      <div>
+        <a href="https://reactjs.org/" target="blank">
+          <i
+            id="skills"
+            title="React"
+            className="devicon-react-original-wordmark colored "
+          ></i>
+        </a>
+        <a href="https://redux.js.org/" target="blank" title="Redux">
+          <Redux />
+        </a>
+        <a href="https://webpack.js.org/" target="blank" title="Webpack">
+          <Webpack />
+        </a>
+        <a href="https://babeljs.io/" target="blank" title="Babel">
+          <Babel />
+        </a>
+      </div>
+      <div>
+        <a href="https://www.w3schools.com/js/" target="blank">
+          <i
+            id="skills"
+            title="JavaScript"
+            className="devicon-javascript-plain colored "
+          ></i>
+        </a>
+        <a href="https://www.w3schools.com/html/html_intro.asp" target="blank">
+          <i
+            id="skills"
+            title="Html5"
+            className="devicon-html5-plain-wordmark colored "
+          ></i>
+        </a>
+        <a href="https://www.w3schools.com/css/" target="blank">
+          <i
+            id="skills"
+            title="Css3"
+            className="devicon-css3-plain-wordmark colored "
+          ></i>
+        </a>
+        <a href="http://lesscss.org/" target="blank">
+          <i
+            id="skills"
+            title="Less"
+            className="devicon-less-plain-wordmark colored"
+          ></i>
+        </a>
+      </div>
+      <div>
+        <a href="https://nodejs.org" target="blank">
+          <i
+            id="skills"
+            title="NodeJS"
+            className="devicon-nodejs-plain colored"
+          ></i>
+        </a>
+        <a href="https://git-scm.com/" target="blank">
+          <i
+            id="skills"
+            title="Git"
+            className="devicon-git-plain-wordmark colored"
+          ></i>
+        </a>
+        <a href="https://jquery.com/" target="blank" title="Jquery">
+          <Jquery />
+        </a>
+        <a href="https://wordpress.com" target="blank">
+          <i
+            id="skills"
+            title="Wordpress/Web Builders"
+            className="devicon-wordpress-plain"
+          ></i>
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://www.adobe.com/products/illustrator.html"
+          target="blank"
+        >
+          <i
+            id="skills"
+            title="Adobe Illustrator"
+            className="devicon-illustrator-plain colored"
+          ></i>
+        </a>
+        <a href="https://jestjs.io/" target="blank" title="Jest">
+          <Jest />
+        </a>
+        <a
+          href="https://www.zelda.com/about/"
+          target="blank"
+          title="The Legend of Zelda"
+        >
+          <Triforce />
+        </a>
+        <a
+          href="https://www.drumeo.com/beat/3-studies-that-prove-drummers-are-the-best/"
+          target="blank"
+          title="Drum set"
+          className="drums"
+        >
+          <img
+            src="http://www.drumchat.com/images/smilies/drumset-3.gif"
+            id="skills"
+          />
+        </a>
+      </div>
+    </div>
+  </Fragment>
 );
